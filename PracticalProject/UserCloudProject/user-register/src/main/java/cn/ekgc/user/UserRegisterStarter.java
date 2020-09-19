fe6b2bc760e00640f3c6fd98fd5f35abd8ec17b0
黄昔 <319732764@qq.com>
2020-09-19 15:27:29 +0800

    完成注册中心项目创建

--- PracticalProject/UserCloudProject/user-register/src/main/java/cn/ekgc/user/UserRegisterStarter.java
+++ PracticalProject/UserCloudProject/user-register/src/main/java/cn/ekgc/user/UserRegisterStarter.java
@@ -1,6 +1,7 @@
 package cn.ekgc.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * <b>注册中心启动器</b>
@@ -8,6 +9,7 @@ import org.springframework.boot.SpringApplication;
  * @version 4.1.0
  * @since 4.1.0
  */
+@SpringBootApplication
 public class UserRegisterStarter {
 	public static void main(String[] args) {
 		SpringApplication.run(UserRegisterStarter.class,args);
